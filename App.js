de1159973a2924ef8c787f71d24063301a9e7edc
Aditya Nagar <nagar.s.aditya@gmail.com>
2021-07-25 21:47:29 +0530

    Timer Cancel Added and Subject History added

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
-import { Focus } from './src/features/focus/Focus'
-import { Timer } from './src/features/TImer/Timer'
-import { colors } from './src/utils/colors'
-import { spacing } from './src/utils/sizes'
+import { Focus } from './src/features/focus/Focus';
+import { Timer } from './src/features/TImer/Timer';
+import { colors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState("coding");
+  const [focusSubject, setFocusSubject] = useState('');
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  useEffect(() => {
+    if (focusSubject) {
+      setFocusHistory([...focusHistory, focusSubject]);
+    }
+  }, [focusSubject]);
 
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Timer focusSubject={focusSubject} onTimerEnd = {()=>
-        setFocusSubject(null)
-        } />
+        <Timer
+          focusSubject={focusSubject}
+          onTimerEnd={() => setFocusSubject(null)}
+          clearSubject={() => setFocusSubject(null)}
+        />
       ) : (
-        <Focus addSubject={setFocusSubject}/>
+        <Focus addSubject={setFocusSubject} />
       )}
       <Text> {focusSubject} </Text>
     </View>
@@ -25,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: spacing.md ,
+    paddingTop: spacing.md,
     backgroundColor: colors.black,
-    color: "white",
+    color: 'white',
   },
 });
